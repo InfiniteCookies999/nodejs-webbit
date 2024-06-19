bfa30650c7913ff7342ec75e2aff979619f77f4e
InfiniteCookies999 <infinitecookies959@gmail.com>
2024-06-19 19:58:03 -0400

    Add create button for posts

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -13,26 +13,44 @@ export default function NavBar() {
     userImgPath = `/static/uploads/users/profile_pictures/${userContext.profileFile}`;
   }
 
+  const showCreatePostBtn = window.location.pathname.startsWith("/w/");
+
   return (
     <nav id="site-navbar">
-      {userContext ?
-        userContext.isLoggedIn ?
-        <a className="right-nav-element">
-          <img src={userImgPath}
-              className="rounded-circle"
-              style={{width:"2.2rem", height:"2.2rem"}}></img>
-        </a> :
-          <a id="login-button" className='right-nav-element' onClick={(e) => {
-            popupContext.setPopup(currentPopup =>
-              ({ ...currentPopup, stateType: PopupType.LOGIN }));
-          }}>
-            <span>Log In</span>
-          </a>
-        // No context loaded.
-        :
-        <a className="rounded-circle right-nav-element"
-          style={{width:"2.2rem", height:"2.2rem", backgroundColor:"gray"}}></a>
-      }
+      <div className='right-nav-element'>
+        {showCreatePostBtn &&
+          <button id="post-create-btn" className='mr-4' onClick={() => {
+            if (!userContext) return;
+
+            if (!userContext.isLoggedIn) {
+              popupContext.setPopup(currentPopup =>
+                ({ ...currentPopup, stateType: PopupType.SIGNUP }));
+            } else {
+              const subname = window.location.pathname.split('/')[2];
+              console.log("subname: ", subname);
+              window.location.href = `/w/${subname}/submit`;
+            }
+          }}>+ Create</button>
+        }
+        {userContext ?
+          userContext.isLoggedIn ?
+          <a>
+            <img src={userImgPath}
+                className="rounded-circle"
+                style={{width:"2.2rem", height:"2.2rem"}}></img>
+          </a> :
+            <a id="login-button" className='right-nav-element' onClick={(e) => {
+              popupContext.setPopup(currentPopup =>
+                ({ ...currentPopup, stateType: PopupType.LOGIN }));
+            }}>
+              <span>Log In</span>
+            </a>
+          // No context loaded.
+          :
+          <a className="rounded-circle"
+            style={{width:"2.2rem", height:"2.2rem", backgroundColor:"gray"}}></a>         
+        }
+      </div>
     </nav>
   );
 }
