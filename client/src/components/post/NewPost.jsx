174b059307f35a52fc30684dbcbe410eecf478dc
InfiniteCookies999 <infinitecookies959@gmail.com>
2024-06-18 21:27:10 -0400

    Create dynamic file uploader for post creation

--- client/src/components/post/NewPost.jsx
+++ client/src/components/post/NewPost.jsx
@@ -14,11 +14,84 @@ function swapUpload(showChildId) {
   for (const child of document.querySelector('.body-container').children) {
     child.hidden = true;
   }
+  document.querySelector('.body-container').style['border-style'] = 'solid';
   document.getElementById(showChildId).hidden = false;
 }
 
-export default function NewPost() {
+function dragFinished(e) {
+  e.target.parentElement.classList.remove('border-primary');
+  const input = document.getElementById('file-input-holder');
+  if (input.files.length > 0) {
+    // Do not remove the solid border if there is already a file being displayed.
+    return;
+  }
+  document.querySelector('.body-container').style['border-style'] = 'dashed';
+}
+
+function updateDisplayImageLeftRightArrows() {
+  const input = document.getElementById('file-input-holder');
+  const imageDisplay = document.getElementById('image-display');
+  const leftCaret = imageDisplay.querySelector('.bx-caret-left');
+  const rightCaret = imageDisplay.querySelector('.bx-caret-right');
   
+  if (input.files.length > 1) {
+    const index = imageDisplay.getAttribute('data-index');
+    leftCaret.hidden = !(index > 0);
+    rightCaret.hidden = !(index < input.files.length - 1);
+  } else {
+    leftCaret.hidden = true;
+    rightCaret.hidden = true;
+  }
+}
+
+function addMediaImage(file) {
+  const input = document.getElementById('file-input-holder');
+  const imageDisplay = document.getElementById('image-display');
+  imageDisplay.hidden = false;
+  document.getElementById('drag-drop-text').hidden = true;
+  document.getElementById('media-drag-drop').classList.remove('media-drag-drop-pointer');
+
+  if (input.files.length === 4) {
+    document.getElementById('add-images').hidden = true;
+  }
+
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = () => {
+    imageDisplay.style.backgroundImage = `url(${reader.result})`;
+    imageDisplay.setAttribute("data-index", input.files.length - 1);
+    document.getElementById('image-display').hidden = false;
+    updateDisplayImageLeftRightArrows();
+  };
+}
+
+function changeMediaImage(newIndex) {
+  const input = document.getElementById('file-input-holder');
+  const imageDisplay = document.getElementById('image-display');
+  
+  imageDisplay.setAttribute("data-index", newIndex);
+
+  const reader = new FileReader();
+  reader.readAsDataURL(input.files[newIndex]);
+  reader.onload = () => {
+    imageDisplay.style.backgroundImage = `url(${reader.result})`;
+    updateDisplayImageLeftRightArrows();
+  };
+}
+
+function removeMediaFile(input, index) {
+  let newFiles = Array.from(input.files);
+                      
+  newFiles.splice(index, 1);
+  const dataTransfer = new DataTransfer(newFiles);
+  for (const f of newFiles) {
+    dataTransfer.items.add(f);
+  }
+  input.files = dataTransfer.files;
+}
+
+export default function NewPost() {
+
   return (
     <div className="container">
       <div className="row">
@@ -41,7 +114,11 @@ export default function NewPost() {
                 <span className="tab" onClick={(e) => {
                   swapUnderline(e.target);
                   swapUpload('media-drag-drop');
-                  
+                  const input = document.getElementById('file-input-holder');
+                  if (input.files.length === 0) {
+                    document.querySelector('.body-container').style['border-style'] = 'dashed';
+                  }
+
                 }}>Media</span>
                 <div className="tab-underline bg-primary" hidden={true}></div>
               </div>            
@@ -65,9 +142,129 @@ export default function NewPost() {
                   }}
                   />
 
-                <div id="media-drag-drop" hidden={true}>
+                <div id="media-drag-drop" hidden={true} className='media-drag-drop-pointer'
+                  onClick={(e) => {
+                    
+                    
+                    if (e.target !== document.getElementById('media-drag-drop') &&
+                        e.target !== document.getElementById('drag-drop-text')) {
+                      return;
+                    }
+                    
+                    const selector = document.getElementById('file-selector');
+
+                    selector.onchange = () => {
+                      const file = selector.files[0];
+                      if (!file.type.startsWith('image/')) {
+                        return;
+                      }
+                      
+                      const input = document.getElementById('file-input-holder');
+                      const transfer = new DataTransfer();
+                      transfer.items.add(file);
+                      input.files = transfer.files;
+                      addMediaImage(file);
+                    };
+                    selector.click();
+                  }}
+                  onDragOver={(e) => {
+                    e.preventDefault();
+                    e.target.parentElement.classList.add('border-primary');
+                    document.querySelector('.body-container').style['border-style'] = 'solid';
+                  }}
+                  onDragEnd={dragFinished}
+                  onDragLeave={dragFinished}
+                  onDrop={(e) => {
+                    e.preventDefault();
+                    e.target.parentElement.classList.remove('border-primary');
+
+                    const input = document.getElementById('file-input-holder');
+
+                    if (input.files.length === 4) {
+                      // Already too many files!
+                      return;
+                    }
+
+                    if (e.dataTransfer.files.length === 0 ||
+                        !e.dataTransfer.files[0].type.startsWith('image/')) {
+                      if (input.files.length === 0) {
+                        document.querySelector('.body-container').style['border-style'] = 'dashed';
+                      }
+                      return;
+                    }
+
+                    const file = e.dataTransfer.files[0];
+
+                    const transfer = new DataTransfer();
+                    for (const f of input.files) {
+                      transfer.items.add(f);
+                    }
+                    transfer.items.add(file);
+                    input.files = transfer.files;
+
+                    addMediaImage(file);
+                    
+                  }}
+                  >
                   <span id="drag-drop-text">Drag and drop media or click to upload</span>
-                  <input type="file" style={{display:"none"}}/>
+                  <input id="file-input-holder" type="file" name="file-holder" multiple="multiple" hidden={true}/>
+                  <input id="file-selector" type="file" hidden={true} />
+
+                  <div id="image-display" data-index="0" hidden={true}>
+                    <span className='bx bx-trash' onClick={() => {
+                      const index = parseInt(document.getElementById('image-display').getAttribute("data-index"));
+                      
+                      const input = document.getElementById('file-input-holder');
+                      
+                      removeMediaFile(input, index);
+
+                      document.getElementById('add-images').hidden = false;
+
+                      if (input.files.length === 0) {
+                        document.querySelector('.body-container').style['border-style'] = 'dashed';
+                        document.getElementById('image-display').hidden = true;
+                        document.getElementById('drag-drop-text').hidden = false;
+                        document.getElementById('media-drag-drop').classList.add('media-drag-drop-pointer');
+                      } else  {
+                        const newIndex = index === 0 ? 0 : index - 1;
+                        changeMediaImage(newIndex);
+                      }
+                    }}></span>
+                    <div id="add-images" onClick={() => {
+                      const selector = document.getElementById('file-selector');
+
+                      selector.onchange = () => {
+                        const file = selector.files[0];
+                        if (!file.type.startsWith('image/')) {
+                          return;
+                        }
+                        
+                        const input = document.getElementById('file-input-holder');
+                        const transfer = new DataTransfer();
+                        for (const f of input.files) {
+                          transfer.items.add(f);
+                        }
+                        transfer.items.add(file);
+                        input.files = transfer.files;
+
+                        addMediaImage(file);
+                      };
+                      selector.click();
+                    }}>
+                      <span className='bx bx-images'></span>
+                      <span id="add-images-text">Add</span>
+                    </div>
+                    <span className='bx bx-caret-left' hidden={true} onClick={() => {
+                      const index = parseInt(document.getElementById('image-display').getAttribute("data-index"));
+                      changeMediaImage(index - 1);
+                    }}></span>
+                    <span className='bx bx-caret-right' hidden={true} onClick={() => {
+                      const index = parseInt(document.getElementById('image-display').getAttribute("data-index"));
+                      changeMediaImage(index + 1);
+                    }}></span>
+                  </div>
+
+                  
                 </div>
             </div>
             <span className='mt-2 bg-primary' id='submit-btn' postActionType="submit">Post</span>
