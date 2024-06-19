37e36e7696c36a3255eceabfef380cd831aa5c5f
InfiniteCookies999 <infinitecookies959@gmail.com>
2024-06-19 19:22:20 -0400

    Display karma and join date for user profile

--- client/src/components/user/User.jsx
+++ client/src/components/user/User.jsx
@@ -34,43 +34,69 @@ export default function User() {
   const profilePicture = user && user.profileFile ? `/static/uploads/users/profile_pictures/${user.profileFile}` :
       '/static/default_user_picture.jpg';
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-3">
+  let cakeDayStr = "";
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  if (user) {
+    const cakeDayDate = new Date(user.createdAt);
+    cakeDayStr += months[cakeDayDate.getMonth()];
+    cakeDayStr += " " + cakeDayDate.getDay();
+    cakeDayStr += " " + cakeDayDate.getFullYear();
+  }
 
-        </div>
-        <div className="col-sm-6">
-          {user ?
-            <>
-              <br />
-              <div id="profile-row">
-                <img src={profilePicture} style={{width:"10rem", height:"12rem"}} />
-                <div id="username-container">  
-                  <span id='username-heading'>{user.username}</span>
-                  <span id="username-heading-sm">{"u/" + user.username}</span>
-                </div>
+  return (
+    <div className="row">
+      <div className="col-sm-3">
+
+      </div>
+      <div className="col-sm-6">
+        {user ?
+          <>
+            <br />
+            <div id="profile-row">
+              <img src={profilePicture} style={{width:"10rem", height:"12rem"}} />
+              <div id="username-container">  
+                <span id='username-heading'>{user.username}</span>
+                <span id="username-heading-sm">{"u/" + user.username}</span>
               </div>
+            </div>
+            <br />
+            <button id="posts-tab-btn" className="tab-btn" style={{backgroundColor:"#aaa"}} onClick={(e) => {
+              setTab(Tabs.POSTS);
+              document.getElementById('posts-tab-btn').style.backgroundColor = '#aaa';
+              document.getElementById('comments-tab-btn').style.backgroundColor = '#ddd';
+            }}>Posts</button>
+            <button id="comments-tab-btn" className="tab-btn" style={{backgroundColor:"#ddd"}} onClick={() => {
+              setTab(Tabs.COMMENTS);
+              document.getElementById('posts-tab-btn').style.backgroundColor = '#ddd';
+              document.getElementById('comments-tab-btn').style.backgroundColor = '#aaa';
+            }}>Comments</button>
+            {tab === Tabs.POSTS ?
+              <PostContainer userId={user.id} /> : <UsersComments user={user} />
+            }
+          </>
+          :
+          <span>Loading...</span>}
+      </div>
+      <div className="col-sm-3">
+        <br />
+        <div id="user-info">
+          <div className="row">
+            <div className="col-sm-4">
+              <span className="user-info-values">{user ? user.postKarma : 0}</span>
               <br />
-              <button id="posts-tab-btn" className="tab-btn" style={{backgroundColor:"#aaa"}} onClick={(e) => {
-                setTab(Tabs.POSTS);
-                document.getElementById('posts-tab-btn').style.backgroundColor = '#aaa';
-                document.getElementById('comments-tab-btn').style.backgroundColor = '#ddd';
-              }}>Posts</button>
-              <button id="comments-tab-btn" className="tab-btn" style={{backgroundColor:"#ddd"}} onClick={() => {
-                setTab(Tabs.COMMENTS);
-                document.getElementById('posts-tab-btn').style.backgroundColor = '#ddd';
-                document.getElementById('comments-tab-btn').style.backgroundColor = '#aaa';
-              }}>Comments</button>
-              {tab === Tabs.POSTS ?
-                <PostContainer userId={user.id} /> : <UsersComments user={user} />
-              }
-            </>
-            :
-            <span>Loading...</span>}
-        </div>
-        <div className="col-sm-3">
-          
+              <span className="user-info-desc">Post Karma</span>
+            </div>
+            <div className="col-sm-4">
+              <span className="user-info-values">{user ? user.commentKarma : 0}</span>
+              <br />
+              <span className="user-info-desc">Comment Karma</span>
+            </div>
+            <div className="col-sm-4">
+              <span className="user-info-values">{cakeDayStr}</span>
+              <br />
+              <span className="user-info-desc">Cake day</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
